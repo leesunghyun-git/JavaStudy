c09120862d28d639fce3a0c7110eb1bd10c75207
simpl <simpl@59.16.253.43>
2025-07-21 21:07:09 +0900

    1일차 자바변수자료형 및 문법연습

--- 자바연습1일차/src/자바연습1일차/자바문법연습.java
+++ 자바연습1일차/src/자바연습1일차/자바문법연습.java
@@ -4,7 +4,20 @@ public class 자바문법연습 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hello Java!!");
+		System.out.println("\"Hello Java!!\"");
+		System.out.println("C:\\JavaStudy\\eclipse");
+		System.out.print("\nJava Study");
+		System.out.print("\nJava\tStudy\n");
+		System.out.println(1000); // 10진법
+		System.out.println(0b1111); // 2진법
+		System.out.println(01111); // 8진법
+		System.out.println(0xa165); // 16진법
+		System.out.println(10.25);
+		System.out.println('자');
+		System.out.println("홍길동");
+		System.out.println(true);
+		System.out.println(false);
 	}
 
 }
