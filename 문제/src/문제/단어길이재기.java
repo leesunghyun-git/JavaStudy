bf5fe69a77178f11c300cca8f21e495e7744c09b
sist123 <sist123@sist>
2025-08-05 17:47:06 +0900

    12일차 javastudy 배열 및 코딩테스트

--- 문제/src/문제/단어길이재기.java
+++ 문제/src/문제/단어길이재기.java
@@ -3,14 +3,12 @@ import java.io.BufferedWriter;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
 public class 단어길이재기 {
 
 	public static void main(String[] args) 
 	throws Exception {
 		BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 		int t=Integer.parseInt(br.readLine());
 		for(int i=0;i<t;i++)
 		{
--- 문제/src/문제/별찍기7.java
+++ 문제/src/문제/별찍기7.java
@@ -1,30 +1,82 @@
+//package 문제;
+//import java.util.Scanner;
+//public class 별찍기7 {
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		Scanner scan=new Scanner(System.in);
+//		int n=scan.nextInt();
+//		for(int i=1;i<=2*n-1;i++)
+//		{
+//			if(i<=n)
+//			{
+//			for(int j=n;j>=i;j--)
+//				System.out.print(" ");
+//			for(int j=1;j<=2*i-1;j++)
+//				System.out.print("*");
+//			System.out.println();
+//			}
+//			else
+//			{
+//			for(int j=0;j<=i-n;j++)
+//			{
+//				System.out.print(" ");
+//			}
+//			for(int j=2*n-1;j>=2*(i-n)+1;j--)
+//			{		
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//			}
+//		}
+//
+//	
+//	}
+//}
+
 package 문제;
 import java.util.Scanner;
+
 public class 별찍기7 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner scan=new Scanner(System.in);
-		int n=scan.nextInt();
-		for(int i=1;i<2*n-1;i++)
-		{
-			if(i<=n)
-			{
-			for(int j=n;j>=i;j--)
-				System.out.print(" ");
-			for(int j=1;j<=2*i-1;j++)
-				System.out.print("*");
-			System.out.println();
-			}
-			else
-			{
-			for(int j=n;j<=i;j++)
-				System.out.print(" ");
-			for(int j=1;j<=2*n-i;j++)
-				System.out.print("*");
-			System.out.println();
-			}
-		}
-	}
-
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        
+        for(int i= 1; i<= 2*n-1;i++)
+        {
+        	if (i <= n)
+        	{
+        		for (int j= 0; j<n-i;j++)
+        			System.out.print(" ");
+        		for (int j= 0; j<2*i-1;j++)
+        			System.out.print("*");
+        		System.out.println();
+        	}
+        	else
+        	{
+        		for (int j=0;j<i-n;j++)
+        			System.out.print(" ");
+        		for (int j=0;j<2*(2*n-i)-1;j++)
+        			System.out.print("*");
+        		System.out.println();
+        	}
+//        for (int i = 1; i <= 2 * n - 1; i++) {
+//            int starCount;
+//            int spaceCount;
+//
+//            if (i <= n) {
+//                spaceCount = n - i;
+//                starCount = 2 * i - 1;
+//            } else {
+//                spaceCount = i - n;
+//                starCount = 2 * (2 * n - i) - 1;
+//            }
+//
+//            for (int j = 0; j < spaceCount; j++)
+//                System.out.print(" ");
+//            for (int j = 0; j < starCount; j++)
+//                System.out.print("*");
+//            System.out.println();
+        }
+    }
 }
